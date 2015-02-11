2a5747358d98d29fd845ac233601a124b2eea251
Reuben Cummings <reubano@gmail.com>
2015-02-12 00:08:57 +0300

    Refactor setup

--- setup.py
+++ setup.py
@@ -6,8 +6,45 @@ Mezmorize
 Adds function memoization support
 
 """
+import sys
 
-from setuptools import setup
+from os import path as p
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+def read(filename, parent=None):
+    parent = (parent or __file__)
+
+    try:
+        with open(p.join(p.dirname(parent), filename)) as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+def parse_requirements(filename, parent=None):
+    parent = (parent or __file__)
+    filepath = p.join(p.dirname(parent), filename)
+    content = read(filename, parent)
+
+    for line_number, line in enumerate(content.splitlines(), 1):
+        candidate = line.strip()
+
+        if candidate.startswith('-r'):
+            for item in parse_requirements(candidate[2:].strip(), filepath):
+                yield item
+        else:
+            yield candidate
+
+requirements = parse_requirements('requirements.txt')
+packages = find_packages(exclude=['tests'])
+
+# Avoid byte-compiling the shipped template
+sys.dont_write_bytecode = True
 
 setup(
     name='Mezmorize',
@@ -18,7 +55,7 @@ setup(
     author_email='thadeusb@thadeusb.com',
     description='Adds function memoization support',
     long_description=__doc__,
-    packages=['mezmorize'],
+    packages=packages,
     zip_safe=False,
     platforms='any',
     test_suite='test_cache',
@@ -35,5 +72,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    install_requires=requirements,
 )
