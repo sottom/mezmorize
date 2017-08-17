d13722f3949412d757a7e7dddb2629fbf173d523
Reuben Cummings <reubano@gmail.com>
2017-08-17 22:18:07 +0300

    Refactor memcache kwarg assigning

--- mezmorize/backends.py
+++ mezmorize/backends.py
@@ -27,7 +27,7 @@ from werkzeug.contrib.cache import (
 from .utils import (
     DEF_MC_SERVERS, HAS_MEMCACHE, AVAIL_MEMCACHES, get_pylibmc_client,
     get_pymemcache_client, get_bmemcached_client, DEF_REDIS_HOST,
-    DEF_REDIS_PORT, DEF_DEFAULT_TIMEOUT)
+    DEF_REDIS_PORT)
 
 try:
     from redis import from_url
@@ -68,9 +68,6 @@ def get_mc_client(module_name, binary=True, **kwargs):
 
 class MemcachedCache(_MemcachedCache):
     def __init__(self, *args, **kwargs):
-        default_timeout = kwargs.pop('default_timeout', DEF_DEFAULT_TIMEOUT)
-        key_prefix = kwargs.pop('key_prefix', None)
-
         if not HAS_MEMCACHE:
             raise RuntimeError('No memcache module found.')
 
@@ -80,14 +77,18 @@ class MemcachedCache(_MemcachedCache):
         if not avail_memcaches:
             raise RuntimeError('No compatible memcache module found.')
 
-        preferred_mc = kwargs.pop('preferred_memcache', 'pylibmc')
+        preferred_mc = kwargs.get('preferred_memcache', 'pylibmc')
 
         if len(avail_memcaches) == 1 or preferred_mc not in avail_memcaches:
             filterer = partial(contains, avail_memcaches)
             preferred_mc = next(filter(filterer, AVAIL_MEMCACHES))
 
-        client = get_mc_client(preferred_mc, **kwargs)
-        skwargs = {'default_timeout': default_timeout, 'key_prefix': key_prefix}
+        whitelist = {'default_timeout', 'key_prefix'}
+        blacklist = whitelist.union(['preferred_memcache'])
+        mkwargs = {k: v for k, v in kwargs.items() if k not in blacklist}
+        skwargs = {k: v for k, v in kwargs.items() if k in whitelist}
+
+        client = get_mc_client(preferred_mc, **mkwargs)
         super(MemcachedCache, self).__init__(servers=client, **skwargs)
         self.TooBig = client.TooBig
         self.client_name = preferred_mc
